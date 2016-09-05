6771a141b07d80a38ab2dc16f4d909f4e6d488ba
Gerardo Suarez <andresuarezz2693@gmail.com>
2016-09-05 10:37:31 -0500

    Segunda Clase: creacion de CardViews con los datos del clima quedamos en el codigo.

--- app/src/main/java/adapter/AdapterPronosticosClima.java
+++ app/src/main/java/adapter/AdapterPronosticosClima.java
@@ -1,37 +1,74 @@
 package adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import icesi.com.climaapp.R;
 
 /**
  * Created by 1143847755 on 02/09/2016.
  */
 public class AdapterPronosticosClima extends RecyclerView.Adapter <AdapterPronosticosClima.ViewHolder> {
+
     private String [] datosClima;
 
+    public AdapterPronosticosClima(String[] datosClima)
+    {
+        this.datosClima = datosClima;
+    }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
-            super(v);
-            mTextView = v;
+    /**
+     * Se "infla" la vista con los elementos del layout que representa cada item del Cardview
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @Override
+    public AdapterPronosticosClima.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        //Relacionar los elementos graficos del cardview con la clase itemView
+        View itemView = LayoutInflater.from( parent.getContext( ) )
+                .inflate( R.layout.cardview_pronostico, parent, false );
+        //Asignar el view que tiene los cardview a nuestra clase ViewHolder
+        ViewHolder viewHolder = new ViewHolder( itemView );
+        //retornar nuestro viewHolder
+        return viewHolder;
+    }
+
+    /**
+     * Relacionar los elementos del arreglo con el viewHolder
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(AdapterPronosticosClima.ViewHolder holder, int position)
+    {
+        holder.descripcionClima.setText ( datosClima[position]);
+    }
+
+    /**
+     * Clase que representa los atributos del CardView
+     */
+    class ViewHolder extends  RecyclerView.ViewHolder
+    {
+        public TextView descripcionClima;
+
+        public ViewHolder(View itemView)
+        {
+            super(itemView);
+            //Inicializar los atributos
+            descripcionClima = (TextView)itemView.findViewById(R.id.texto_descripcion_clima);
         }
     }
 
     @Override
-    public AdapterPronosticosClima.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(AdapterPronosticosClima.ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
+    public int getItemCount()
+    {
+        return datosClima.length;
     }
 }
--- app/src/main/java/icesi/com/climaapp/Activity/MainActivity.java
+++ app/src/main/java/icesi/com/climaapp/Activity/MainActivity.java
@@ -6,50 +6,37 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import adapter.AdapterPronosticosClima;
 import icesi.com.climaapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Parametros
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private AdapterPronosticosClima adapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Inicializar el recyclerView
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        //mRecyclerView.setHasFixedSize(true);
 
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
+
+        //Setear el layoutManager
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        String  [] datosClima = {"Jueves: 22c°", "Viernes 35°C", "Sábado 35°C","Domingo 40°C", "Lunes 35°C"};
-        // specify an adapter (see also next example)
-        mAdapter = new RecyclerView.Adapter(datosClima) {
-            @Override
-            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        String  [] datosClima = {"Jueves: 22c°", "Viernes 35°C", "Sábado 35°C","Domingo 40°C", "Lunes 35°C", "Martes 44°c" ,"Miércoles 45°C"};
 
-            }
+        //Inicializar el Adapter con la lista de datos del clima
+        adapter = new AdapterPronosticosClima(datosClima);
 
-            @Override
-            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
-            }
-
-            @Override
-            public int getItemCount() {
-                return 0;
-            }
-        };
-        mRecyclerView.setAdapter(mAdapter);
-
-
+        //asignar el adapter al RecyclerView
+        mRecyclerView.setAdapter(adapter);
     }
 }
